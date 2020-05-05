1df896a87b25cc28e960cecc7ae3faef139a22d0
sialexsofficial <64383709+sialexsofficial@users.noreply.github.com>
2020-05-05 15:18:41 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$headers1 = "X-User-Id: Facebook_117016829994554";
+$headers2 = "X-Raw-User-Id: 4642e4fa-7d35-447b-954c-b33d930e8065";
+$headers3 = "X-User-Name: Dilex";
+$headers4 = "X-Invite-Code: FXE96jTSc";
+$headers5 = "X-User-Token: AbwOgbCO6UFg4SNV";
+$headers6 = "X-Own-Mac: 3c%3Ab6%3Ab7%3A2f";
+$headers7 = "X-Android-Id: c3c0f462aa9f6a22";
+$headers8 = "X-Device-Type: samsung++SM-J105F";
+
+
+?>
